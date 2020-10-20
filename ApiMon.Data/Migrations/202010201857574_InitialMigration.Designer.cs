4503f1da417692ece83e4263b5e46b62b1c97871
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-10-20 15:00:35 -0400

    Added Post Controller for Element Type

--- ApiMon.Data/Migrations/202010201857574_InitialMigration.Designer.cs
+++ ApiMon.Data/Migrations/202010201857574_InitialMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace ApiMon.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class InitialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "202010201703518_initial"; }
+            get { return "202010201857574_InitialMigration"; }
         }
         
         string IMigrationMetadata.Source
