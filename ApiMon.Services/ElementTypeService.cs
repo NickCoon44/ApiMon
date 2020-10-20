3c24229b43e454b8935574a364018c6697eebebe
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-10-20 13:05:44 -0400

    Element services updated

--- ApiMon.Services/ElementTypeService.cs
+++ ApiMon.Services/ElementTypeService.cs
@@ -12,6 +12,8 @@ namespace ApiMon.Services
     {
         private ApplicationDbContext _context = new ApplicationDbContext();
 
+
+        //Get All
         public IEnumerable<ElementTypeListItem> GetElementTypes()
         {
             var query = _context.ElementTypes.Select(e => new ElementTypeListItem
@@ -20,6 +22,49 @@ namespace ApiMon.Services
                 Name = e.Name
             });
             return query;
+        }
+
+        //Create
+        public bool CreateElementType (ElementTypeCreate model)
+        {
+            var entity = new ElementType()
+            {
+                Name = model.Name,
+                Advantages = model.Advantages,
+                Disadvantages = model.Disadvantages
+            };
+
+            _context.ElementTypes.Add(entity);
+
+            return _context.SaveChanges() == 1;
+        }
+
+        //Get by ID
+        public ElementTypeDetail GetElementTypeById(int id)
+        {
+            var entity = _context.ElementTypes.Single(e => e.Id == id);
+
+            var model = new ElementTypeDetail()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Advantages = entity.Advantages,
+                Disadvantages = entity.Disadvantages
+            };
+
+            // TODO Add in Monsters and Moves Later
+
+            return model;
+        }
+
+        public bool UpdateElementType(int id, ElementTypeUpdate model)
+        {
+            var entity = _context.ElementTypes.Single(e => e.Id == id);
+            //If no Id found return false
+            if (entity is null)
+                return false;
+
+            return true;
         }
     }
 }
