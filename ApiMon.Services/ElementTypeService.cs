cdfaea8e2b6ce864f1009501455b88cb2410cd2d
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-10-20 12:24:57 -0400

    added get all Element Types

---
+++ ApiMon.Services/ElementTypeService.cs
@@ -0,0 +1,25 @@
+﻿using ApiMon.Data;
+using ApiMon.Models.ElementTypeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiMon.Services
+{
+    class ElementTypeService
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        public IEnumerable<ElementTypeListItem> GetElementTypes()
+        {
+            var query = _context.ElementTypes.Select(e => new ElementTypeListItem
+            {
+                Id = e.Id,
+                Name = e.Name
+            });
+            return query;
+        }
+    }
+}
