5f2ab6f4cf681a735957929aff8f8eb9165d129a
Nicholas <higherfuryx@mail.com>
2020-10-20 12:21:45 -0500

    list to strings on Move class

--- ApiMon.Data/ElementType.cs
+++ ApiMon.Data/ElementType.cs
@@ -13,8 +13,8 @@ namespace ApiMon.Data
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public List<string> Advantages { get; set; }
-        public List<string> Disadvantages { get; set; }
+        public string Advantages { get; set; }
+        public string Disadvantages { get; set; }
         public virtual ICollection<Monster> Monsters { get; set; }
         public virtual ICollection<Move> Moves { get; set; }
     }
