cc5bfcfa0db13e77f26cd57b9fa5a9650bce4bb8
Nicholas <higherfuryx@mail.com>
2020-10-20 14:31:53 -0500

    Move service and controller for post and getall

--- ApiMon.Services/MoveService.cs
+++ ApiMon.Services/MoveService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApiMon.Data;
+using ApiMon.Models.MoveModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,43 @@ namespace ApiMon.Services
 {
     public class MoveService
     {
+        public bool CreateMove(MoveCreate model)
+        {
+            var entity =
+                new Move()
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    ElementTypeId = model.ElementTypeId
+                };
 
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Moves.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<MoveListItem> GetMoves()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Moves
+                        .Select(
+                            e =>
+                                new MoveListItem
+                                {
+                                    Id = e.Id,
+                                    Name = e.Name,
+                                    Description = e.Description,
+                                    ElementType = e.ElementType.Name
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
     }
 }
